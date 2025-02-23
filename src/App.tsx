69a45ed1740e12519d5b193d534769b0cdfaba33
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-02-23 23:52:52 +0900

    add pseudo selector

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ function App() {
       <div className="container">
         <h1 className={style.title}>CSS Modules Playground</h1>
         <h2 className={style.subtitle}>Sub Title</h2>
+        <p>
+          <a className={style.link} href="/">
+            Sample Link
+          </a>
+        </p>
       </div>
     </>
   )
